8657496f12cd34ad9f4fe0aad4a036ba67d39893
SSSS-1-source <1686284290@qq.com>
2025-10-20 20:56:23 +0800

    Update code

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://SSSS-1-source.github.io",
+	base: "/JMQ.us.kg",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
